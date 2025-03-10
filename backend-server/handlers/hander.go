42322c251b655f67d8ed4c914210b5fb9fdca92f
PythonHacker24 <adityapatil24680@gmail.com>
2025-03-10 22:58:50 +0530

    added slog for better logging

---
+++ backend-server/handlers/hander.go
@@ -0,0 +1,8 @@
+package handlers
+
+import (
+    "log"
+    "net/http"
+)
+
+
--- backend-server/main.go
+++ backend-server/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-    "fmt"
-//     "net/http"
+	"fmt"
+	"log/slog"
+	"net/http"
+    "os"
+
+    "backend-server/config"
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Ok")
+}
 
 func main() {
-   fmt.Println("Hello World!") 
+    opts := &slog.HandlerOptions{Level: slog.LevelWarn}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, opts))
+
+    mux := http.NewServeMux()
+
+    // Frontend Handlers 
+    mux.HandleFunc("/health", healthHandler)
+
+    // Backend Handlers
+
+    logger.Info("Server Started Listening", "Host", config.Host, "Port", config.Port)
+    if err := http.ListenAndServe(fmt.Sprintf("%s:%s", config.Host, config.Port), mux); err != nil {
+        slog.Error("Failed to start server at port 8080", err)
+    }
 }
